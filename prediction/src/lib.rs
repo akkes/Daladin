424faecd6fc67eb5f33201ea7affddb988649078
uj <sudo.screenlife@gmail.com>
2016-01-09 15:19:40 +0100

    B D D D C Makrov class en train d'être faite. Constructeur OK. Là je regarde comment utiliser fold() pour faire la somme dans la matrice pour toujours être égal à u32::MAxC

--- prediction/src/lib.rs
+++ prediction/src/lib.rs
@@ -1,8 +1,8 @@
 ///Represents a Makrov chain.
 struct Makrov {
     number_of_chains: u32,
-    sensibility: f32,
-    values: [[f32; number_of_chains]; number_of_chains]
+    sensibility: u32,
+    values: Vec<Vec<u32>>,
 }
 impl Makrov {
     ///Creates a new Makrov chain.
@@ -10,18 +10,35 @@ impl Makrov {
     ///
     /// * `number_of_chains` - Number of chains the Makrov chain will have. Each chain can be a podcast ID.
     /// * `sensibility` - This will afflict how much the probability values will evolve depending on the user feedback.
-    fn new(number_of_chains: u32, sensibility: f32) -> Makrov {
+    fn new(number_of_chains: u32, sensibility: u32) -> Makrov {
         Makrov {
             number_of_chains : number_of_chains,
             sensibility : sensibility,
-            /*for i in 0..number_of_chains-1 {
-
-            }*/
+            values : Makrov::initValues(number_of_chains),
         }
     }
+
+    fn initValues(number_of_chains: u32) -> Vec<Vec<u32>> {
+        return vec![vec![std::u32::MAX / number_of_chains; number_of_chains as usize]; number_of_chains as usize]
+    }
+
+    fn printValues(&self) {
+        println!("{:?}", self.values);
+    }
+
+    fn isValid(&self) -> bool {
+        return true
+    }
+    /*fn fillValues(self) {
+        self.values = vec![vec![200, 100, 500], vec![200, 200, 600], vec![100, 100, 800]];
+    }*/
 }
 #[test]
 fn it_works() {
-    let hello = "Hello!";
-    println!("{:?}", hello);
+    let mTest = Makrov::new(4, 10);
+    mTest.printValues();
+    let sum = (1..5).fold(0, |sum, x| sum + x);
+    //sum +=
+    println!("{:?}", sum);
+    //println!("{:?}", hello);
 }
--- prediction/target/debug/prediction.d
+++
@@ -1,5 +0,0 @@
-/home/uinelj/dev/Daladin/prediction/target/debug/prediction.d: src/lib.rs
-
-/home/uinelj/dev/Daladin/prediction/target/debug/libprediction.rlib: src/lib.rs
-
-src/lib.rs:
