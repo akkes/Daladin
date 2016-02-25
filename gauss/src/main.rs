e2761d6a77e9c0408005b0248a1534a4dff54988
uj <aulien.jbadji@gmail.com>
2016-02-25 15:16:25 +0100

    gauss function works with Tm and Duration structs.

--- gauss/src/main.rs
+++ gauss/src/main.rs
@@ -11,6 +11,17 @@ fn gauss(hour : f64, p : f64, normal_hour : f64, duration : f32) -> f64{
     let egblog = a*(top_exp/bot_exp).exp();
     return egblog;
 }
+
+fn gaussTm(hour : time::Tm, p : f64, normal_hour : time::Tm, duration : time::Duration) -> f64{
+    //tada
+    //A tweak, marche chelou pour les contenus courts
+    let a = p;
+    let top_exp = -((normal_hour-hour).num_seconds() as f64).powi(2);
+    let bot_exp = 2f64 * (duration.num_seconds() as f64/4f64).powi(2);
+    let egblog = a*(top_exp/bot_exp).exp();
+    return egblog;
+}
+
 fn get_actual_time() {
     println!("{:?}", time::get_time());
 }
@@ -23,9 +34,11 @@ fn main() {
 
     //println!("{:?}", tm);
     println!("Hello, world!");
-    let initial_time = time::strptime("0-08-00-00", "%w-%H-%M-%S").unwrap();
-    let initial_time2  = time::strptime("0-08-10-00", "%w-%H-%M-%S").unwrap();
-    println!("{:?}", initial_time2-initial_time);
-
-    println!("{}", gauss(7.85, 0.6, 8.0, 1.5));
+    let initial_time = time::strptime("0-07-00-00", "%w-%H-%M-%S").unwrap();
+    let initial_time2  = time::strptime("0-08-00-00", "%w-%H-%M-%S").unwrap();
+    let duration = time::Duration::minutes(90);
+    //println!("{:?}", initial_time2-initial_time);
+    //println!("{:?}", initial_time2);
+    println!("{}", gauss(7.0, 0.6, 8.0, 1.5));
+    println!("{}", gaussTm(initial_time, 0.6, initial_time2, duration));
 }
