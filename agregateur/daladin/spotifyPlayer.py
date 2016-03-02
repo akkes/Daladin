5fea6b81b18bf82858f9fd1daa5ac251543742ed
Louis Desportes <louis@akkes.fr>
2016-03-03 00:37:47 +0100

    fix mistakes in spotifyPlayer

--- agregateur/daladin/spotifyPlayer.py
+++ agregateur/daladin/spotifyPlayer.py
@@ -1,8 +1,13 @@
 import threading
+import spotify
 
 
 class SpotifyPlayer(object):
     """docstring for SpotifyPlayer"""
+    def connection_state_listener(session):
+            if self.session.connection.state is spotify.ConnectionState.LOGGED_IN:
+                logged_in_event.set()
+
     def __init__(self, arg):
         super(SpotifyPlayer, self).__init__()
         self.spotifyURI = arg
@@ -10,19 +15,15 @@ class SpotifyPlayer(object):
         self.config.user_agent = 'Daladin Radio '
         self.config.tracefile = b'/tmp/libspotify-trace.log'
         self.config.load_application_key_file('daladin_spotify.key')
-        self.session = spotify.Session(config)
-        loop = spotify.EventLoop(session)
+        self.session = spotify.Session(self.config)
+        loop = spotify.EventLoop(self.session)
         loop.start()
         self.session.on(
             spotify.SessionEvent.CONNECTION_STATE_UPDATED,
-            connection_state_listener)
+            self.connection_state_listener)
         audio = spotify.AlsaSink(self.session)
         self.session.login('alice', 's3cr3tp4ssw0rd')
         logged_in_event = threading.Event()
-
-    def connection_state_listener(session):
-            if self.session.connection.state is spotify.ConnectionState.LOGGED_IN:
-                logged_in_event.set()
 
     def preload(self):
         pass
@@ -31,5 +32,5 @@ class SpotifyPlayer(object):
         logged_in_event.wait()
         for track in self.listing.tracks:
             track.load()
-            session.player.load(track)
-            session.player.play()
+            self.session.player.load(track)
+            self.session.player.play()
--- agregateur/daladin/spotifyPlaylistPlayer.py
+++ agregateur/daladin/spotifyPlaylistPlayer.py
@@ -5,7 +5,7 @@ class SpotifyPlaylistPlayer(spotifyPlayer.SpotifyPlayer):
     """docstring for SpotifyPlaylistPlayer"""
     def __init__(self, arg):
         super(SpotifyPlaylistPlayer, self).__init__(arg)
-        self.playlist = session.get_playlist(self.spotifyURI)
+        self.playlist = self.session.get_playlist(self.spotifyURI)
 
     def preload(self):
         self.playlist.load()
