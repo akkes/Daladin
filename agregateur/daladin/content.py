daa3613495d5e651a51c6bfd43d2b391e3a52813
Louis Desportes <louis@akkes.fr>
2016-03-05 17:58:42 +0100

    fix timeout on content get error

--- agregateur/daladin/content.py
+++ agregateur/daladin/content.py
@@ -5,7 +5,6 @@ import json
 import re
 from urlparse import urlparse
 import soundPlayer
-"""Just a song for now."""
 # TODO: Clean the way you access to genres, maybe with a property.
 
 
@@ -13,48 +12,52 @@ class Content:
     def __init__(self, uri):  # name to be removed
         # init properties
         self.uri = uri
-        self.name = None
-        self.artist = None
-        self.duration = None  # TODO : Spotify API
+        self.name = ""
+        self.artist = ""
+        self.duration = 0  # TODO : Spotify API
 
         # clean URI
         uri = soundPlayer.cleanURI(uri)
+        print "cleaned uri: " + uri
 
         # get properties
-        if re.match("^spotify:playlist:", uri):
-            playlist = soundPlayer.session.get_playlist(uri).load()
-            self.name = playlist.name
-        elif re.match("^spotify:album:", uri):
-            album = soundPlayer.session.get_album(uri).load()
-            self.name = album.name
-            self.artist = album.artist.load().name
-        elif re.match("^spotify:artist:", uri):
-            artist = soundPlayer.session.get_artist(uri).browse.load()
-            self.artist = artist.load().name
-            self.name = self.artist
-        elif re.match("^spotify:track:", URI):
-            track = soundPlayer.session.get_track(uri).load()
-            self.artist = track.artists
-            self.name = track.name
+        try:
+            if re.match("^spotify:playlist:", uri):
+                playlist = soundPlayer.session.get_playlist(uri).load()
+                self.name = playlist.name
+            elif re.match("^spotify:album:", uri):
+                album = soundPlayer.session.get_album(uri).load()
+                self.name = album.name
+                self.artist = album.artist.load().name
+            elif re.match("^spotify:artist:", uri):
+                artist = soundPlayer.session.get_artist(uri).browse.load()
+                self.artist = artist.load().name
+                self.name = self.artist
+            elif re.match("^spotify:track:", URI):
+                track = soundPlayer.session.get_track(uri).load()
+                self.artist = track.artists
+                self.name = track.name
 
-        # get genres
-        self.genres = []
-        if urlparse(uri).scheme == "spotify":
-            response = urllib2.urlopen("http://developer.echonest.com" +
-                                       "/api/v4/artist/profile" +
-                                       "?api_key=ETXPUYCZFFTUMUV0D&name='" +
-                                       urllib.quote_plus(self.artist) +
-                                       "'&bucket=genre&format=json").read()
-            json_response = json.loads(response)
-            if json_response['response']['status']['code'] != 0:
-                print(str(json_response['response']['status']['code']) +
-                      ": " + json_response['response']['status']['message'])
-                self.genres = [None]
-            else:
-                for genre in json_response['response']['artist']['genres']:
-                    print(genre["name"])
-                    self.genres.append(genre["name"])
-                print("\n")
+            # get genres
+            self.genres = []
+            if urlparse(uri).scheme == "spotify":
+                response = urllib2.urlopen("http://developer.echonest.com" +
+                                           "/api/v4/artist/profile" +
+                                           "?api_key=ETXPUYCZFFTUMUV0D&name='" +
+                                           urllib.quote_plus(self.artist) +
+                                           "'&bucket=genre&format=json").read()
+                json_response = json.loads(response)
+                if json_response['response']['status']['code'] != 0:
+                    print(str(json_response['response']['status']['code']) +
+                          ": " + json_response['response']['status']['message'])
+                    self.genres = [None]
+                else:
+                    for genre in json_response['response']['artist']['genres']:
+                        print(genre["name"])
+                        self.genres.append(genre["name"])
+                    print("\n")
+        except soundPlayer.spotify.error.Timeout:
+            print "spotify is too slow!"
 
     def similarity(self, other):
         score = 0
